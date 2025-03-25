d0272bac4829f2ffb2fe43c4ca9acf6e7bb895f0
kumanboy <husandavronov13@gmail.com>
2025-03-25 17:20:13 +0500

    navbar end

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,14 @@ import Navbar from "./components/Navbar.jsx";
 
 const App = () => {
     const [frameZoom, setFrameZoom] = useState(false)
+    const [activePage, setActivePage] = useState(0);
 
     const toggleZoom = () => {
         setFrameZoom(!frameZoom);
+    }
+
+    const handleNavClick = (pageIndex) =>{
+        setActivePage(pageIndex);
     }
 
     return (
@@ -17,7 +22,7 @@ const App = () => {
                 max-h-[90vh] border border-gray-300 rounded-2xl resize overflow-auto relative transition-all duration-300
                 "`}
             >
-                <Navbar/>
+                <Navbar activePage={activePage} handleNavClick={handleNavClick} />
                 <Controls toggleZoom={toggleZoom} frameZoom={frameZoom} />
             </div>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import {navItems} from "../data/index.js";
 
-const Navbar = () => {
+const Navbar = ({activePage, handleNavClick}) => {
     return (
-        <div>Navbar</div>
+        <div className="2xl:w-80 xl:w-52 w-44 h-full bg-blue-600 flex flex-col justify-between pt-5 pl-6 pb-14 md:pb-0">
+            <a href={"#"} className={"2xl:text-2xl xl:text-xl font-bold text-white md-14 tracking-wider"}>
+                Apple Products
+            </a>
+            <div className={"flex flex-col flex-grow"}>
+                {navItems.map((item, index) => (
+
+                    <a href={"#"} key={Math.random()}
+                       className={"xl:w-36 lg:w-36 w-30 flex items-center justify-between my-3.5 text-left cursor-pointer"}
+                       onClick={() => handleNavClick(index)}>
+                        <i className={`${item.icon} text-xl text-white`}></i>
+                        <span className={"text-sm text-gray-50 mr-auto mx-2.5 tracking-wider"}>{item.label}</span>
+                        <i className={`text-lg text-yellow-400 ${activePage === index ? item.activeIcon : item.inactiveIcon}`}></i>
+                    </a>
+                ))}
+            </div>
+            <div className={"flex justify-between pr-6 pb-2"}>
+                <a href={"#"}>
+                    <i className={"bx bxl-instagram text-2xl text-white"}></i>
+                </a>
+                <a href={"#"}>
+                    <i className={"bx bxl-telegram text-2xl text-white"}></i>
+                </a>
+                <a href={"#"}>
+                    <i className={"bx bxl-youtube text-2xl text-white"}></i>
+                </a>
+
+            </div>
+        </div>
     )
 }
 export default Navbar
